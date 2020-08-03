d21ecf249b57293e7fa664c0e11c85b82d061ef0
Rafael Lino Lobo <52603609+losfodo@users.noreply.github.com>
2020-08-03 08:39:43 -0300

    Add files via upload

---
+++ js/bootstrap/bootstrap1/7galeria/_js/script.js
@@ -0,0 +1,8 @@
+
+            $(function() {/*unidade 9 u9 scripts*/
+                $('.carousel').carousel({
+                    interval:2000,/*por 2 segundos troca de foto*/
+                    pause: "null",/*sem pause nas trocas- usa jquery*/
+                });
+            });
+ 
