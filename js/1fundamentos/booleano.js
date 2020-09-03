2e97ae6cc051bb65cb70f4a783ee6334a2de759a
Rafael Lino Lobo <rafaelzinholobo@gmail.com>
2020-09-03 20:27:10 -0300

    updates

--- js/1fundamentos/booleano.js
+++ js/1fundamentos/booleano.js
@@ -5,7 +5,7 @@ isAtivo = true
 console.log(isAtivo)
 
 isAtivo = 1
-console.log(!!isAtivo)//!!=oposto do oposto ou contrario==da true
+console.log("1 !!isAtivo é " + !!isAtivo)//!!=oposto do oposto ou contrario==da true 
 
 isAtivo = 1
 console.log(!isAtivo)//da false
@@ -18,11 +18,11 @@ console.log(!false)//!=não + false=daria true
 
 console.log('verdadeiros abaixo...')
 console.log(!!' ')//string com espaço true
-console.log(!!Infinity)//com letra maiuscula começa,,A propriedade global Infinity é um valor numérico que representa infinito.
+console.log('infinity é ' + !!Infinity)//com letra maiuscula começa,,A propriedade global Infinity é um valor numérico que representa infinito.
 console.log(!!(isAtivo = true))
 console.log(!!(isAtivo=30))
 
-console.log('falsos abaixoxo...')
+console.log('falsos abaixo...')
 console.log(!!0)
 console.log(!!'')//sem espaço ou nada escrito false
 console.log(!!null)
